3bd1dc9c16b8b4221efeed4e498e73203d1aa476
munadirkhan <rdetonate@gmail.com>
2026-03-20 05:07:50 -0400

    fix: PlayerAttack uses G key and 2D physics, awards combat XP

--- Assets/Scripts/Core/PlayerAttack.cs
+++ Assets/Scripts/Core/PlayerAttack.cs
@@ -1,31 +1,43 @@
 using UnityEngine;
 
+// Munadir: Updated attack key from Space to G for combat
+// Munadir: Switched from 3D Physics.OverlapSphere to 2D Physics2D.OverlapCircleAll
+// Munadir: Added ProgressionSystem combat XP call on successful hit
 public class PlayerAttack : MonoBehaviour
 {
     public float attackRange = 2f;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        // Munadir: Changed from Space to G key for attacking enemies
+        if (Input.GetKeyDown(KeyCode.G))
         {
-            Collider[] hits = Physics.OverlapSphere(transform.position, attackRange);
+            // Munadir: Using 2D overlap circle instead of 3D - this is a 2D game
+            Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, attackRange);
 
-            foreach (Collider hit in hits)
+            foreach (Collider2D hit in hits)
             {
                 if (hit.CompareTag("Enemy"))
                 {
                     PlayerWeapon weapon = GetComponent<PlayerWeapon>();
                     int damage = weapon.GetDamage();
-
                     Debug.Log("Hit enemy for " + damage + " damage");
 
                     EnemyHealth eh = hit.GetComponent<EnemyHealth>();
                     if (eh != null)
-                    {
                         eh.TakeDamage(damage);
-                    }
+
+                    // Munadir: Award combat XP through ProgressionSystem on each kill
+                    GameManager.Instance?.progressionSystem?.AddCombatXP();
                 }
             }
         }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // Munadir: Visualize attack range in Scene view for debugging
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
     }
 }
