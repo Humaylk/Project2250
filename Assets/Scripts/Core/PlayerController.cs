ec365545ac10c64ccd73831804a0e6171e4ec0df
munadirkhan <rdetonate@gmail.com>
2026-03-20 05:22:56 -0400

    fix: PlayerController uses GetAxisRaw and normalized vector for clean 2D movement

--- Assets/Scripts/Core/PlayerController.cs
+++ Assets/Scripts/Core/PlayerController.cs
@@ -1,16 +1,18 @@
 using UnityEngine;
 
+// Munadir: Fixed movement axes for 2D top-down game
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
 
     void Update()
     {
-        float moveX = Input.GetAxis("Horizontal");
-        float moveY = Input.GetAxis("Vertical");
+        // Munadir: GetAxisRaw gives clean input without smoothing
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
 
-        Vector3 move = new Vector3(moveX, moveY, 0);
-
+        // Munadir: Normalized so diagonal movement isnt faster
+        Vector2 move = new Vector2(moveX, moveY).normalized;
         transform.Translate(move * speed * Time.deltaTime);
     }
 }
