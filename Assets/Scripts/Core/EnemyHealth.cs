e0dc134e8e7f30a49da5f32551a298d5c4d13eb3
Yoseph <yosephelshareif2023@gmail.com>
2026-03-25 13:43:34 -0400

    Fixed the animations for the enemy for level 1 and Changed the code to match what is needed to play the animation. -Yoseph, Munadir, Humayl, Shaan

--- Assets/Scripts/Core/EnemyHealth.cs
+++ Assets/Scripts/Core/EnemyHealth.cs
@@ -3,38 +3,37 @@ using UnityEngine;
 // Munadir: Fixed Die() to use 2D Collider instead of 3D Collider
 // Munadir: Added UIManager combat XP notification on death
 // Munadir: Added null checks to prevent missing component errors
+//Yoseph: Added a code to validate death so that a death animation may be played
 public class EnemyHealth : MonoBehaviour
 {
     public int health = 50;
-
+    Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
     public void TakeDamage(int damage)
     {
         health -= damage;
         Debug.Log("Enemy Health: " + health);
-
-        // Munadir: Show remaining health as hint via UIManager
         GameManager.Instance?.uiManager?.ShowHint("Golem HP: " + health);
-
         if (health <= 0)
             Die();
     }
-
     void Die()
     {
         Debug.Log("Enemy Died!");
-
-        // Munadir: Award combat XP when golem is killed
+        animator.SetBool("isDead", true);
+        animator.ResetTrigger("Attack");
+        GolemAI ai = GetComponent<GolemAI>();
+        
+        if (ai != null) ai.enabled = false;
+        
         GameManager.Instance?.progressionSystem?.AddCombatXP();
         GameManager.Instance?.uiManager?.ShowHint("Golem defeated! +10 XP");
-
-        // Munadir: Fixed - using Collider2D instead of 3D Collider
         Collider2D col = GetComponent<Collider2D>();
+        
         if (col != null) col.enabled = false;
-
-        // Munadir: Fixed - using SpriteRenderer instead of Renderer
-        SpriteRenderer sr = GetComponent<SpriteRenderer>();
-        if (sr != null) sr.enabled = false;
-
-        Destroy(gameObject, 0.1f);
+        Destroy(gameObject, 1.5f);
     }
 }
